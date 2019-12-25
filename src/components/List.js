8c53b7fc4d1eb25fab0bffc2d2a97d4b3d05dba3
Argenis De Los Santos <argenis04_09@icloud.com>
2019-12-24 23:51:56 -0700

    added the list component

---
+++ src/components/List.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class List extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Video list here</h1>
+            </div>
+        )
+    }
+}
