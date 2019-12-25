652bc39c34a8fe2a9108dbfea1785c98fa57ffeb
Argenis De Los Santos <argenis04_09@icloud.com>
2019-12-24 23:48:22 -0700

    updated the app.js component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import youtube from './api/youtube';
+import { Grid } from '@material-ui/core';
 
 class App extends Component {
   render() {
     return (
-      <div className='main-container'>
-        <h1>Start Here</h1>
-      </div>
+   <Grid justify='center' container spacing={16}>
+     <Grid item xs={12}>
+       <Grid container spacing={16}>
+        <Grid item xs={12}>
+         {/* {more coming soon} */}
+        </Grid>
+          <Grid item xs={8}>
+            {/* {more coming soon} */}
+          </Grid>
+          <Grid item xs={4}>
+            {/* {video here} */}
+          </Grid>
+       </Grid>
+     </Grid>
+   </Grid>
     )
   }
 };
