0bb310e6d651911a5e624b7eeb80fbf22ca90c51
Argenis De Los Santos <argenis04_09@icloud.com>
2019-12-25 12:27:27 -0700

    modified the search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
+import { Paper, TextField } from '@material-ui/core';
 
 export default class Search extends Component {
     state = {
         searchItem: '',
     }
+    
+    handleChange = (e) => {
+        this.setState({ searchItem: e.target.value })
+    }
+   
     render() {
         return (
-            <div>
-                <h1>Search here</h1>
-            </div>
+           <Paper elevation={6} style={{ padding: '25px'}}>
+               <form onSubmit={this.handleSubmit}>
+                   <TextField fullWidth label='Search...' onChange={this.handleChange} />
+               </form>
+           </Paper>
         )
     }
 }
