7a952322fe7b47cfd4033ec10e1c6ad0817e277e
Argenis De Los Santos <argenis04_09@icloud.com>
2019-12-27 22:02:00 -0700

    modified the details components

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Paper, Typography } from '@material-ui/core';
 
-const Details = ({video}) => {
+const Details = ({ video }) => {
+    if(!video) return <div>Loading...</div>
+
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
     return(
      <>
      <Paper elevation={6} style={{height: '70%'}}>
-
+       <iframe frameBorder='0' height='100%' width='100%' title='Video Player' src={videoSrc} />
      </Paper>
      <Paper elevation={6} style={{padding: '15px'}}>
          
