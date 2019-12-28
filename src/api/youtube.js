fbe5016b0d565a0ea3be8a46b1287d975bf759fa
Argenis De Los Santos <argenis04_09@icloud.com>
2019-12-27 22:35:07 -0700

    modified the youtube component

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
-    responseType: "json",
-    kind: "youtube#video"
+    baseURL: 'https://www.googleapis.com/youtube/v3'
 })
